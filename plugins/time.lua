1c8a1bc43f630078a52c69ed094b1608a0e3c71f
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-12 12:21:51 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,25 +1,12 @@
---[[
-#
-#    Time And Date
-#
-#    @Dragon_born
-#  @GPMod
-#
-#
-]]
-
 function run(msg, matches)
-local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
-local jdat = json:decode(url)
-local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    —--\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@SpheroCh\nTnx to\nGPmod'
-return text
+text = io.popen("date && cal"):read('*all')
+  return text
 end
 return {
-description = "time",
-usage = {
-"!time : show time and date",
-},
-  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
-run = run 
+  patterns = {
+    '^[!/][Dd]ate$',
+    '^[Tt]ime$',
+  },
+  run = run,
+  moderated = true
 }
