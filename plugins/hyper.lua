6ab688207a28663176c41790e9b0ddf5f7682f57
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-19 15:26:43 +0430

    Update hyper.lua

--- plugins/hyper.lua
+++ plugins/hyper.lua
@@ -3,8 +3,14 @@ local text = matches[1]
   send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
+description = "text to markdown",
+usage = {
+"!markdown *text* : text to bold",
+"!markdown [text](link) : link to hyperlinkhyperlink",
+"!markdown _text_ : text to italic",
+},
 patterns = {
-"^/hyper (.*)"
+"^[!/]markdown (.*)"
 },
 run = run 
 }
