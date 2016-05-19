779fc09ddf0c2f9c9de0962d6f4c964c1e675c7d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-19 17:52:11 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -5,7 +5,7 @@ do
     local name_log = user_print_name(msg.from)
         if matches[1] == 'support' then
         local group_link = data[tostring(support)]['settings']['set_link']
-    return "[click here to join Support]("..group_link..")"
+    return "[click here to join Support]("..group_link..")
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
     end
 end
