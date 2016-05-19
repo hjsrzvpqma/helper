ea8c3f30c8815ed892df8616d62c84a94408e8a5
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-19 18:36:02 +0430

    Update txt.lua

--- plugins/txt.lua
+++ plugins/txt.lua
@@ -1,10 +1,15 @@
 function run(msg, matches)
-text = io.popen("cat " .. matches[1]):read('*all')
-  return text
+if not is_sudo(msg) then
+local text = '*'..msg.from.print_name..'*_You're not Sudo_'
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
+text = io.popen("cat" ..matches[1]):read('*all')
+  local text = '_'..text..'_'
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
   patterns = {
-    '^[Tt]xt (.*)$'
+    '^[#/!]txt (.*)$'
   },
   run = run,
   moderated = true
