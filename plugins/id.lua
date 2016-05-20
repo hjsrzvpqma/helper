6309b67ca873634b81f681cb941a424e0c50333a
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-20 10:34:37 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,22 +1,6 @@
 do function run(msg, matches)
-if matches[1]:lower() == 'id' and is_sudo(msg) then
-local text = "*[Sudo]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' and is_admin(msg) then
-local text = "*[Admin]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' and is_owner(msg) then
-local text = "*[Owner]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' and is_momod(msg) then
-local text = "*[Moderator]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
 if matches[1]:lower() == 'id' then
-local text = "*[Member️]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
+local text = *Your ID* : _"..msg.from.id"_\n*Group ID* : _"msg.to.id"_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 end
