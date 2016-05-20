a5dc92273b728d5eec06ef6cd689e0368a50c587
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-20 10:40:34 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,6 +3,10 @@ if matches[1]:lower() == 'id' then
 local text = "*Your ID* : _"..msg.from.id"_\n*Group ID* : _"msg.to.id"_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
+if matches[1]:lower() == 'id' and is_sudo(msg) then
+local text = "*my Sudo*\n*Your ID* : _"..msg.from.id"_\n*Group ID* : _"msg.to.id"_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
 end
 return {
 description = "show your id",
