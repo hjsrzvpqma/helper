e407c1d743250fec3bc75b215a064fbe6a308bd6
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-20 10:27:54 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,32 +1,29 @@
 do function run(msg, matches)
-if matches[1]:lower() == 'id' and is_sudo(msg) then
-local text = "*[Sudo]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+
+	  if matches[1]:lower() == 'id' and is_sudo(msg) then
+      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' and is_admin(msg) then
+      return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' and is_owner(msg) then
+      return "Owner☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' and is_momod(msg) then
+      return "Moderator☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' then
+      return "Member☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+    end
 end
-if matches[1]:lower() == 'id' and is_admin(msg) then
-local text = "*[Admin]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' and is_owner(msg) then
-local text = "*[Owner]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' and is_momod(msg) then
-local text = "*[Moderator]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-if matches[1]:lower() == 'id' then
-local text = "*[Member️]*\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-end
-return {
-description = "show your id",
-usage = "!id : show your userid and information",
-patterns = {
-"^[!/#]([Ii][Dd])$",
-"^[Ii][Dd]$",
-},
-run = run
+  return {
+  description = "show your id",
+  usage = "me",
+  patterns = {
+  "^[!/#]([Ii][Dd])$",
+    },
+  run = run
 }
 end
+
+--sepehr blacklifeTM
