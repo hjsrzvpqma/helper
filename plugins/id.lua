ae618833d95757930fef8508706d742aa2b35f4d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-20 10:07:00 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,15 +1,19 @@
 do function run(msg, matches)
 if matches[1]:lower() == 'id' and is_sudo(msg) then
-return "[Sudo]️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
+local text = "[Sudo](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 if matches[1]:lower() == 'id' and is_admin(msg) then
-return "#Admin️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
+local text = "[Admin](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 if matches[1]:lower() == 'id' and is_owner(msg) then
-return "#Owner️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
+local text = "[Owner](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 if matches[1]:lower() == 'id' and is_momod(msg) then
-return "#Moderator️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
+local text = "[Moderator](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 if matches[1]:lower() == 'id' then
 local text = "[Member️](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_"
