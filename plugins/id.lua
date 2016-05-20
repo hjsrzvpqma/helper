471ff8ad8395f04f682d6aeb0471c5a41168ab72
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-20 09:56:36 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -12,7 +12,7 @@ if matches[1]:lower() == 'id' and is_momod(msg) then
 return "#Moderator️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
 end
 if matches[1]:lower() == 'id' then
-local text = "#Member️\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."
+local text = "#Member️\n*Your ID* : _"..msg.from.id.."_\n*Your name* : _"..msg.from.print_name.."_\n*Group name* : _"..msg.to.title.."_\n*Group ID* : _"..msg.to.id.."_\n*msg ID* : _"..msg.id.."_\n*Your msg* : _"..msg.text.."_
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 end
