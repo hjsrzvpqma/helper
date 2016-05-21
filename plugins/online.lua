2f954d274b755436b2871ff9d55fbb289d01393a
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-21 18:09:37 +0430

    Update online.lua

--- plugins/online.lua
+++ plugins/online.lua
@@ -2,7 +2,6 @@ do
 
 function run(msg, matches)
   local text = [[ [Sphero](https://telegram.me/spohero) _And_ [Helper](http://telegram.me/spherorobot) _Is Online Now!_]]
-reply_msg(reply_id, info, ok_cb, false)
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
