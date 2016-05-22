ab811c2a11f7a4c903ef07852fef928bc46a5d32
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-22 10:58:05 +0430

    Update feedback2.lua

--- plugins/feedback2.lua
+++ plugins/feedback2.lua
@@ -31,10 +31,10 @@ local fuses = '!printf user#id' .. msg.from.id
 
 
 
-                 local sends0 = send_msg('chat# 138776903 ', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat# 110380705 ', fuse, ok_cb, false)
 
 
- return 'your feedback succesfully recived to support spherobot :)'
+ return 'Succesfully Send!'
 
 
 
